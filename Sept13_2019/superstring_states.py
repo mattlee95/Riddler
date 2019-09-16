8b2641386f3bf2ff0b2bfcba16de2cbc7f5e5f7e
Matt Lee <mattlee@Matts-iMac-4.local>
2019-09-16 16:58:51 -0700

    cleaning up

--- Sept13_2019/superstring_states.py
+++ Sept13_2019/superstring_states.py
@@ -1,7 +1,3 @@
-
-DEBUG_LEVEL = 0
-
-
 # States and Territories
 STATE_LIST = ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY']
 
@@ -11,10 +7,13 @@ STATE_LIST = ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 
 '''
 Level Structure
 [("current string", [unused_state_list]), ...]
+
+
 '''
 
 current_level = []
 holder_level = []
+
 
 
 def create_next_level():
@@ -63,8 +62,8 @@ def get_longest_string_for_state(state):
             lowest_level += 1
             lowest_level_list = list(current_level)
         else:
-            for i in lowest_level_list:
-                print i[0]
+            '''for i in lowest_level_list:
+                print i[0]'''
             return (lowest_level, lowest_level_list)
             
 
@@ -87,8 +86,9 @@ def run_for_all():
             pass
 
     print "Longest String: {0}".format(current_longest)
-    print current_longest_list
-
+    #print current_longest_list
+    for unique_string in current_longest_list:
+        print unique_string[0]
 
 
 
